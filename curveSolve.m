88a29bf23a4936c08e31d433edf3e805a7ef74b6
Carlos Andrés Arévalo <37085951+carevalor@users.noreply.github.com>
2022-06-01 16:13:03 -0500

    Add files via upload

--- curveSolve.m
+++ curveSolve.m
@@ -1,17 +1,15 @@
 function [q] = curveSolve(r)
-
-    l(1) = 89.45;
-    l(2) = 105.95;
-    l(3) = 100;
-    l(4) = 107.6;
-    offset = atan(100/35);
-    q1 = zeros(max(size(r))/4,1);
-    q2 = zeros(max(size(r))/4,1);
-    q3 = zeros(max(size(r))/4,1);
-    q4 = zeros(max(size(r))/4,1);
-    for i = 1:max(size(r))/4
-        [q1(i) q2(i) q3(i) q4(i)] = inverKinematics(offset, r(4*i-3:4*i,:),l);
-    end
-    q = [q1 q2 q3 q4];
-    
+l(1) = 89.45;
+l(2) = 105.95;
+l(3) = 100;
+l(4) = 107.6;
+offset = atan(100/35);
+q1 = zeros(max(size(r))/4,1);
+q2 = zeros(max(size(r))/4,1);
+q3 = zeros(max(size(r))/4,1);
+q4 = zeros(max(size(r))/4,1);
+for i = 1:max(size(r))/4
+    [q1(i) q2(i) q3(i) q4(i)] = inverKinematics(offset, r(4*i-3:4*i,:),l);
+end
+q = [q1 q2 q3 q4];
 end
--- inverKinematics.m
+++ inverKinematics.m
@@ -1,17 +1,15 @@
 function [q1, q2, q3, q4] = inverKinematics(offset, poseTCP, L)
+q1 = atan2(poseTCP(2,4),poseTCP(1,4));
 
-    q1 = atan2(poseTCP(2,4),poseTCP(1,4));
-    
-    w = poseTCP(1:3,4) - L(4)*poseTCP(1:3,3)- L(1)*[0 0 1]'; 
-    
-    q_3 = acos((w'*w -(L(2)^2+L(3)^2))/(2*L(2)*L(3)));
-    
-    q_2 = acos(-(L(3)^2-(w'*w+L(2)^2))/(2*sqrt(w'*w)*L(2)));
-    
-    
-    q3 = -q_3+offset;
-    q2 = atan2(w(3),norm(w(1:2)))+q_2-offset;
-    
-    q4 = acos([cos(q1) sin(q1) 0]*poseTCP(1:3,3))-q3-q2;
+w = poseTCP(1:3,4) - L(4)*poseTCP(1:3,3)- L(1)*[0 0 1]';
+
+q_3 = acos((w'*w -(L(2)^2+L(3)^2))/(2*L(2)*L(3)));
+
+q_2 = acos(-(L(3)^2-(w'*w+L(2)^2))/(2*sqrt(w'*w)*L(2)));
+
+
+q3 = -q_3+offset;
+q2 = atan2(w(3),norm(w(1:2)))+q_2-offset;
+q4 = acos([cos(q1) sin(q1) 0]*poseTCP(1:3,3))-q3-q2;
 end
 
